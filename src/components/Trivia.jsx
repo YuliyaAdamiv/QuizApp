7f830abc25c2eb6a5575ad723b6e6dc0dd40c6ae
Yuliya <yulua03071992@gmail.com>
2023-03-13 17:56:24 +0200

    CSS animation using React.js, Creating quiz app logics

---
+++ src/components/Trivia.jsx
@@ -0,0 +1,37 @@
+import React, {useEffect, useState} from 'react';
+
+export default function Trivia({
+  data,
+  setTimeout,
+  questionNumber,
+  setQuestionNumber,
+}) {
+  const [question, setQuestion] = useState(null);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [className, setClassName] = useState('answer');
+  useEffect(() => {
+    setQuestion(data[questionNumber - 1]);
+  }, [data, questionNumber]);
+  const handlerClick = (a) => {
+    setSelectedAnswer(a);
+    setClassName('answer active');
+    setTimeout(() => {
+      setClassName(a.correct ? 'answer correct' : 'answer wrong');
+    }, 3000);
+  };
+  return (
+    <div className="trivia">
+      <div className="question">{question?.question}</div>
+      <div className="answers">
+        {question?.answers.map((a) => (
+          <div
+            className={selectedAnswer === a ? className : 'answer'}
+            onClick={() => handlerClick(a)}
+          >
+            {a.text}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
