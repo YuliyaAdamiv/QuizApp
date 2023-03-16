be74ebd762a1e62d764ec77cc3a7521e96fe5a11
Yuliya <yulua03071992@gmail.com>
2023-03-16 12:22:20 +0200

    add sound and user to React app

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,4 +1,8 @@
 import React, {useEffect, useState} from 'react';
+import useSound from 'use-sound';
+import play from '../assets/play.mp3';
+import correct from '../assets/correct.mp3';
+import wrong from '../assets/wrong.mp3';
 
 export default function Trivia({
   data,
@@ -9,6 +13,12 @@ export default function Trivia({
   const [question, setQuestion] = useState(null);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [className, setClassName] = useState('answer');
+  const [letsPlay] = useSound(play);
+  const [correctAnswer] = useSound(correct);
+  const [wrongAnswer] = useSound(wrong);
+  useEffect(() => {
+    letsPlay();
+  }, [letsPlay]);
   useEffect(() => {
     setQuestion(data[questionNumber - 1]);
   }, [data, questionNumber]);
@@ -23,12 +33,18 @@ export default function Trivia({
     delay(3000, () =>
       setClassName(a.correct ? 'answer correct' : 'answer wrong')
     );
-    delay(6000, () => {
+    delay(5000, () => {
       if (a.correct) {
-        setQuestionNumber((prev) => prev + 1);
-        setSelectedAnswer(null);
+        correctAnswer();
+        delay(1000, () => {
+          setQuestionNumber((prev) => prev + 1);
+          setSelectedAnswer(null);
+        });
       } else {
-        setStop(true);
+        wrongAnswer();
+        delay(1000, () => {
+          setStop(true);
+        });
       }
     });
   };
